0336eb479ed59f13abaeb7d8e0b2939760ddc9ab
raufhassan <raufhassan41@gmail.com>
2022-08-04 20:14:18 +0500

    smart contract deployment using web3 Js

--- src/transactions.js
+++ src/transactions.js
@@ -16,18 +16,18 @@ const account2 = "0x5742C31fd10CC557620691F990d991589BE23A6F";
   web3.eth.accounts.create()
   */
 const pk1 = Buffer.from(privateKey1, "hex");
-const pk2 = Buffer.from(privateKey2, "hex");
 
+/* 
+Balance checking of account
+*/
 /* web3.eth.getBalance(account1, (err, bal) =>
   console.log("balance 1 :", web3.utils.fromWei(bal, "ether"))
 );
-
-web3.eth.getBalance(account2, (err, bal) =>
-  console.log("balance 2 :", web3.utils.fromWei(bal, "ether"))
-); */
+*/
 
 // all the units of transaction must be in hex
 web3.eth.getTransactionCount(account1, (err, txCount) => {
+  // tx object for value transaction from one account to another.
   const txObject = {
     nonce: web3.utils.toHex(txCount),
     to: account2,
@@ -35,6 +35,14 @@ web3.eth.getTransactionCount(account1, (err, txCount) => {
     gasLimit: web3.utils.toHex(21000),
     gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
   };
+
+  // tx object to deploy smart contract to ether test network.
+  /* const txObject = {
+    nonce: web3.utils.toHex(txCount),
+    gasLimit: web3.utils.toHex(21000),
+    gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
+    data: '' // smart contract data (in hexadecimal) can be taken from remix.ethereum.org
+  }; */
 
   // Sign the transaction
   const tx = new Tx(txObject);
@@ -46,5 +54,6 @@ web3.eth.getTransactionCount(account1, (err, txCount) => {
   // Broadcast the transaction
   web3.eth.sendSignedTransaction(raw, (err, txHash) => {
     console.log("txHash:", txHash);
+    // use this txHash to find on etherscan
   });
 });
